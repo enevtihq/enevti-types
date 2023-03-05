ba035fb697c559e96b52d3aea552b6258a62c472
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-03-05 22:05:05 +0700

    add socialRaffleConfig type

---
+++ chain/social_raffle/config.ts
@@ -0,0 +1,9 @@
+export type SocialRaffleConfig = {
+  socialRaffle: {
+    maxPrice: string;
+    rewardsCutPercentage: number;
+    blockInterval: number;
+    maxRaffledPerCollection: number;
+    maxRaffledPerProfile: number;
+  };
+};
