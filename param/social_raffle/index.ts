530d6586542bc95191b4fe32fa69004cabb6d549
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-03-07 10:42:37 +0700

    add setcollectionraffleconfig type

--- param/social_raffle/index.ts
+++ param/social_raffle/index.ts
@@ -6,6 +6,11 @@ export type GetCollectionRaffleConfig = {
   id: Buffer;
 };
 
+export type SetCollectionRaffleConfig = {
+  id: Buffer;
+  activated: boolean;
+};
+
 export type GetRecordParam = {
   height: number;
 };
