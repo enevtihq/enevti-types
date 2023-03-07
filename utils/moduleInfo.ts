541b25ec8dbeeef29b06fc9f5c74a99873ae9a25
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-03-07 11:35:43 +0700

    fix module info type

--- utils/moduleInfo.ts
+++ utils/moduleInfo.ts
@@ -2,5 +2,5 @@ import { AssetSchemaFromType } from "./schema";
 
 export type ModuleInfo<T extends string = string> = {
   id: number;
-  asset: Record<T, { id: number; schema: AssetSchemaFromType<any> }>;
+  asset: Record<T, { id: number; schema: AssetSchemaFromType<any, any> }>;
 };
