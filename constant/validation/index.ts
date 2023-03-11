693ce830b683fb4589d5d78aea17173b7d88163a
Aldo Suhartono Putra <aldhosutra@gmail.com>
2023-03-11 21:01:26 +0700

    add validation constant

---
+++ constant/validation/index.ts
@@ -0,0 +1,7 @@
+export const KEY_STRING_MAX_LENGTH = 64;
+export const ID_STRING_MAX_LENGTH = 64;
+export const ID_BYTES_MAX_LENGTH = 32;
+export const CID_STRING_MAX_LENGTH = 59;
+export const ADDRESS_BYTES_MAX_LENGTH = 20;
+export const PUBLIC_KEY_STRING_MAX_LENGTH = 64;
+export const SIGNATURE_STRING_MAX_LENGTH = 128;
